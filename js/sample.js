8a39a8eb4853d41e21b158f2ccd9e443eb43394a
KeitoFurusawa <wisdomsound2.0.0@gmail.com>
2023-04-08 00:12:48 +0900

    ver 0.1.2
    add header and footer
    menu func and hamburger menu

--- js/sample.js
+++ js/sample.js
@@ -109,3 +109,16 @@ function ringoBtn() {
     }
 }
 
+function toggleNav() {
+    var body = document.body;
+    var hamburger = document.getElementById('js-hamburger');
+    var blackBg = document.getElementById('js-black-bg');
+  
+    hamburger.addEventListener('click', function() {
+      body.classList.toggle('nav-open');
+    });
+    blackBg.addEventListener('click', function() {
+      body.classList.remove('nav-open');
+    });
+  }
+  toggleNav();
