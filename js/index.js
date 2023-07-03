fc60a40119ac78ba43b2ce956f3ddc93af3448c2
KeitoFurusawa <wisdomsound2.0.0@gmail.com>
2023-07-03 21:46:16 +0900

    Merge branch 'debug'

--- js/index.js
+++ js/index.js
@@ -1,15 +1,3 @@
-/* グローバル変数 */
-const owner = "ringo";
-const DEBUG_MODE = false; //debug時はここをtrueに
-
-let base_youtube_url;
-let base_youtube_url_live;
-let cnt = 0;
-const mo_videoIds = [];
-
-//console.log(excryption('AIzaSyDcwk2MtvrO63sLn1WwGq1ahlYG30BcnA4', owner.length));
-//console.log(decryption(, owner.length));
-
 function getRandomInt() {
     return Math.floor(Math.random() * max);
 }
@@ -34,15 +22,14 @@ function decryption(string, key) {
     return result;
 }
 
-function setAPIKey(debug_mode) {
-    if (debug_mode) { //止まったら切り替え可能
-        return decryption(Y_API_KEY_D1, owner.length); 
-        //return decryption(Y_API_KEY_D2, owner.length);
-    } else {
-        return decryption(K_LIST[0], owner.length);
-    }
-}
-const APIKEY = setAPIKey(DEBUG_MODE);
+
+////* debug tools *////
+/* debug variable */
+const owner = "ringo";
+const DEBUG_MODE = false;
+let base_youtube_url;
+let base_youtube_url_live;
+let cnt = 0;
 
 const checkbox = document.getElementById('switch');
 checkbox.addEventListener('click', ()=> {
@@ -64,7 +51,11 @@ checkbox2.addEventListener('click', ()=> {
     window.location.reload();
 });
 
-
+/* run debug functions */
+checkDevMode();
+checkBlkAPI();
+//console.log(excryption('AIzaSyDcwk2MtvrO63sLn1WwGq1ahlYG30BcnA4', owner.length));
+//console.log(decryption(, owner.length));
 
 /* define debug functions */
 function enable_dev_mode() {
@@ -121,15 +112,17 @@ function checkDevMode() {
         //console.log(base_youtube_url);
     }
 }
-checkDevMode();
 
 function retCheckDev() {
     let result = localStorage.getItem('dev_mode');
+    console.log("run retCheckDev()");
     if (result != "true") {
         return false;
     }
     return true;
+    
 }
+
 
 function checkBlkAPI() {
     let result = localStorage.getItem('blk_api');
@@ -140,7 +133,6 @@ function checkBlkAPI() {
     } 
     return false;
 }
-checkBlkAPI();
 
 function checkLightMode() {
     let result = localStorage.getItem('lightmode');
@@ -235,68 +227,107 @@ function removeDataSizeAttribute() {
       }
     }
 }
+  
 removeDataSizeAttribute(); // 初回の実行
 
-var video_m_obj_list = []; //グローバル宣言
-var video_l_obj_list = []; //グローバル宣言
-async function push_mo_ul(video_u_obj_list) {
-    try {
-        const response  = await $.ajax({
-            url: 'https://www.googleapis.com/youtube/v3/playlistItems',
-            type: 'GET',
-            dataType: 'json',
-            data: {
-            part: 'snippet',
-            maxResults: 50, // 取得する動画の最大数（50まで）
-            playlistId: "PLCUfW5KwcvZFZi5ytVenKhtKIJP42ZQZx",
-            key: APIKEY
-            }
-        });
-        const items = response.items;
-        //video_u_obj_list = await checkVideoStatus("ZxNsc2aZ7xo", true, "TEST", video_u_obj_list); debug用
-        for (var i = 0; i < items.length; i++) {
-            video_u_obj_list = await checkVideoStatus(items[i].snippet.resourceId.videoId, true, items[i].snippet.title, video_u_obj_list);
-            //mo_videoIds.push(items[i].snippet.resourceId.videoId);
-        }
-        
-        await setMOVideo();
-        await setUCVideo(video_u_obj_list);
-        await setLVideo();
-    } catch (error) {
-        console.log('Error: Failed to retrieve member-exclusive videos.', error);
-    }   
-}
 
-async function checkVideoStatus(videoId, mo, title = "ERROR", videoUObjList = []) {
-    try {
-        const data = await $.ajax({
+function push_mo_ul(video_u_obj_list) {
+    
+    console.log("runnnnn");
+    // チャンネルIDとAPIキーを設定
+    var channelId = 'UCm-nZofnh3_1s_l2Gq3G1KQ';
+    var KEY = decryption(Y_API_KEY_D1, owner.length);
+    var apiKey = KEY;
+    var mo_videoIds = [];
+
+    // メンバー限定動画の再生リストを取得するリクエスト
+    $.ajax({
+        url: 'https://www.googleapis.com/youtube/v3/playlistItems',
+        type: 'GET',
+        dataType: 'json',
+        data: {
+        part: 'snippet',
+        maxResults: 50, // 取得する動画の最大数（50まで）
+        playlistId: "PLCUfW5KwcvZFZi5ytVenKhtKIJP42ZQZx",
+        key: apiKey
+        }
+    }).done(function(data) {
+        // メンバー限定動画を列挙
+        var items = data.items;
+        var videoId;
+        var videoTitle;
+        
+        for (var i = 0; i < items.length; i++) {
+            videoId = items[i].snippet.resourceId.videoId;
+            mo_videoIds.push(videoId);
+            videoTitle = items[i].snippet.title;
+            console.log(videoTitle);
+        }
+    }).fail(function(data) {
+        // エラーハンドリング
+        console.log('Error: Failed to retrieve member-exclusive videos.');
+    });
+
+    for (let j = 0; j < mo_videoIds.length; j++) {
+        console.log("Ajax2");
+        $.ajax({
             url: 'https://www.googleapis.com/youtube/v3/videos',
             type: 'GET',
             dataType: 'json',
             data: {
                 part: 'liveStreamingDetails',
-                id: videoId,
-                key: APIKEY
+                id: mo_videoIds[j],
+                key: apiKey
+            },
+            success: function(data) {
+                // 応答データの処理
+                var liveStreamingDetails = data.items[0].liveStreamingDetails;
+                console.log(data.items[0]);
+                if (liveStreamingDetails && liveStreamingDetails.actualStartTime) {
+                    var startTime = new Date(liveStreamingDetails.actualStartTime);
+                    console.log('配信開始時間:', startTime);
+                    // 動画の情報を表示するなどの処理を行う
+                    console.log("Video ID:", videoId);
+                    //console.log("Video Title:", videoTitle);
+                    video_u_obj_list.push({videoid: videoId, time: startTime});
+                    //サムネを追加する処理
+                    console.log(mo_videoIds);
+                    var ID = mo_videoIds[j];
+                    var TN_URL = `https://img.youtube.com/vi/${ID}/maxresdefault.jpg`;
+                    console.log(TN_URL);
+                    var DATETIME = startTime;
+                    var year = DATETIME.getFullYear() ;	// 年
+                    var month = DATETIME.getMonth() + 1 ;	// 月
+                    var day = DATETIME.getDate() ;	// 日
+                    var hour = DATETIME.getHours() ;	// 時
+                    var minute = DATETIME.getMinutes() ;	// 分
+                    var second = DATETIME.getSeconds() ;	// 秒
+                    var dayOfWeek = DATETIME.getDay() ;	// 曜日(数値)
+                    var dayOfWeekStr = [ "日", "月", "火", "水", "木", "金", "土" ][dayOfWeek] ;	// 曜日(日本語表記)
+                    var DATETIME_formated = `${month}/${day} (${dayOfWeekStr}) ${hour}:${minute.toString().padStart(2, '0')}`;
+                    //$("#youtubeList_u_inner").append(''); /////
+
+                    //配信予定のアイテムを表示
+                    if (checkLightMode()) {
+                        //developer mode
+                        //console.log("https://www.youtube.com/watch?v=" + ID);
+                        $("#youtubeList_u_inner").append('<div class="iframe_wrapper"><a href=' + 'https://www.youtube.com/watch?v=' + ID + ' target="_blank" rel="noopener noreferrer"><img class="inner-logo" src="./src/logo/youtube_social_icon_red.png"><h2 class="sc-time" style="color:black">' +DATETIME_formated + '</h2><img class="thumb" src=' + TN_URL + '></a></div>'); /////
+                    } else {
+                        //nomal mode
+                        $("#youtubeList_u_inner").append('<div class="iframe_wrapper"><h2 class="sc-time">' +DATETIME_formated + '</h2><iframe src="https://www.youtube.com/embed/' + ID + '" frameborder="1" sandbox="allow-scripts allow-popups allow-forms allow-same-origin allow-popups-to-escape-sandbox allow-downloads allow-modals" allowfullscreen></iframe></div>'); /////
+                    }
+                    
+                } else {
+                    console.log('配信情報がありません');
+                }
+            },
+            error: function(error) {
+                // エラーの処理
+                console.log('エラー:', error);
             }
         });
-        
-        const liveStreamingDetails = data.items[0].liveStreamingDetails;
-        //console.log(title, liveStreamingDetails);
-        if (liveStreamingDetails.actualEndTime && liveStreamingDetails.actualStartTime) { //終了済みのメン限配信
-            let startTime = new Date(liveStreamingDetails.actualStartTime);
-            video_m_obj_list.push({videoid: videoId, time: startTime});
-        } else if (liveStreamingDetails.actualStartTime) { //配信中のメン限配信
-            let startTime = new Date(liveStreamingDetails.actualStartTime);
-            video_l_obj_list.push({videoid: videoId, time: startTime, title: title, mo: mo});
-        } else { //配信予定のメン限
-            let startTime = new Date(liveStreamingDetails.scheduledStartTime);
-            videoUObjList.push({videoid: videoId, time: startTime, mo: mo});
-        }
-        return videoUObjList;
-    } catch (error) {
-        console.log('エラー:', error);
-        return videoUObjList;
     }
+        
 }
 
 function setLVideo() {
