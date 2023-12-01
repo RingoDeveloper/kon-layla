226bc2dfb222352899ca12a3edfe72ce31067b2e
KeitoFurusawa <121685362+RingoDeveloper@users.noreply.github.com>
2023-12-01 20:45:26 +0900

    ver 0.6.3 update

--- js/poty2022.js
+++ js/poty2022.js
@@ -1,22 +1,3 @@
-function convert() {
-    let srcElm = document.getElementById("sorce");
-    let time_hElm = document.getElementById("timecode_h");
-    let time_mElm = document.getElementById("timecode_m");
-    let time_sElm = document.getElementById("timecode_s");
-    let resultElm = document.getElementById("result");
-
-    console.log(time_hElm.value);
-    console.log(time_mElm.value);
-    console.log(time_sElm.value);
-
-    let totalSec = (parseInt(time_hElm.value) * 3600) + (parseInt(time_mElm.value) * 60) + parseInt(time_sElm.value);
-    let url = srcElm.value + "?t=" + totalSec;
-    resultElm.textContent = url;
-    resultElm.setAttribute('href', url);
-}
-
-
-
 function check_localStorage() {
     let result = localStorage.getItem('showed-result');
     console.log(result);
