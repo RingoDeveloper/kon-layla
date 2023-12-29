bbfa4b477356c5c0806b2f92b225791392f679b4
RingoDeveloper <wisdomsound2.0.0@gmail.com>
2023-12-29 09:06:27 +0900

    ver 0.7.0 patch 2

--- js/loty2023.js
+++ js/loty2023.js
@@ -12,7 +12,7 @@ function calculateRemainTime() {
 
 /* 投票期間中かどうか返す関数 */
 function checkTerm(termId) {
-    var now = new Date();
+    var now = new Date('2024-01-01T00:01:00+09:00');
     if (termId == 1) {
         return (now > startDateV1 && now < startDateV2); //第一回投票期間かどうか？
     } else if (termId == 2) {
